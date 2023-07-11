3664b54087f5dbbc9a945edc317f2d0bcf578a15
heinemann44 <40678096+heinemann44@users.noreply.github.com>
2023-07-11 22:02:10 +0000

    Adicionado script ´lint:ckeck´ e ´lint:fix´

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação de um blog como exercício do https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <img src="https://i.imgflip.com/7qpml0.jpg" alt="new"></img>
+function Home() {
+  return <img src="https://i.imgflip.com/7qpml0.jpg" alt="new"></img>;
 }
 
 export default Home;
