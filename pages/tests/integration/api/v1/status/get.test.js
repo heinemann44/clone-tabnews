96bf22ab6b91dfe048b231fc71e1a4b8ede3a9be
Samuel Heinemann <samuelehp04@gmail.com>
2024-02-09 19:44:23 -0300

    test: troca de 127.0.0.1 para localhost

--- pages/tests/integration/api/v1/status/get.test.js
+++ pages/tests/integration/api/v1/status/get.test.js
@@ -1,11 +1,11 @@
 test("GET to /api/v1/status shoud return 200", async () => {
-  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
 });
 
 test("GET to /api/v1/status shoud return valid updated_at", async () => {
-  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
   const responseBody = await response.json();
 
@@ -15,7 +15,7 @@ test("GET to /api/v1/status shoud return valid updated_at", async () => {
 });
 
 test("GET to /api/v1/status shoud return database version", async () => {
-  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
   const responseBody = await response.json();
 
@@ -23,7 +23,7 @@ test("GET to /api/v1/status shoud return database version", async () => {
 });
 
 test("GET to /api/v1/status shoud return database max connections", async () => {
-  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
   const responseBody = await response.json();
 
@@ -31,7 +31,7 @@ test("GET to /api/v1/status shoud return database max connections", async () => 
 });
 
 test("GET to /api/v1/status shoud return database open connections", async () => {
-  const response = await fetch("http://127.0.0.1:3000/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
 
   const responseBody = await response.json();
 
