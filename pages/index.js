1109f53ad728641b43c91e4cb68345726dfdb00b
Samuel Heinemann <samuelehp04@gmail.com>
2024-07-09 16:46:42 -0300

    :broom: cleanup(eslint): Ajuste de linting para qualidade de código

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import Image from "next/image";
+
 function Home() {
-  return <img src="https://i.imgflip.com/7qpml0.jpg" alt="new"></img>;
+  return <Image src="https://i.imgflip.com/7qpml0.jpg" alt="new"></Image>;
 }
 
 export default Home;
