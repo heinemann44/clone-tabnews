12bb787a656de2385448e5c0d41ba7ef999ba6d2
Samuel Heinemann <samuelehp04@gmail.com>
2024-09-12 18:32:21 -0300

    test(maintenance): adiciona um padrão para escrita dos testes

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,29 +1,19 @@
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeEach(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;");
+  await orchestrator.clearDatabase();
 });
 
-test("GET to /api/v1/migrations shoud return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+describe("GET to /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      const responseBody = await response.json();
 
-  expect(response.status).toBe(200);
-});
-
-test("GET to /api/v1/migrations shoud return a array", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-
-  const responseBody = await response.json();
-
-  expect(Array.isArray(responseBody)).toEqual(true);
-});
-
-test("GET to /api/v1/migrations shoud return a array not empty", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-
-  const responseBody = await response.json();
-
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(response.status).toBe(200);
+      expect(Array.isArray(responseBody)).toEqual(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,45 +1,46 @@
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeEach(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;");
+  await orchestrator.clearDatabase();
 });
 
-test("POST to /api/v1/migrations shoud return 201", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST to /api/v1/migrationsa", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        const responseBody = await response.json();
+
+        expect(response.status).toBe(201);
+        expect(Array.isArray(responseBody)).toEqual(true);
+      });
+
+      test("For the second time", async () => {
+        const responseMigrationRunned = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(responseMigrationRunned.status).toBe(201);
+
+        const responseMigrationEmpty = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(responseMigrationEmpty.status).toBe(200);
+      });
+    });
   });
-
-  expect(response.status).toBe(201);
-});
-
-test("POST to /api/v1/migrations shoud return a array", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  const responseBody = await response.json();
-
-  expect(Array.isArray(responseBody)).toEqual(true);
-});
-
-test("POST to /api/v1/migrations shoud return 200 when there is no migrations to be runned", async () => {
-  const responseMigrationRunned = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
-
-  expect(responseMigrationRunned.status).toBe(201);
-
-  const responseMigrationEmpty = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
-
-  expect(responseMigrationEmpty.status).toBe(200);
 });
