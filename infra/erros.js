aafe28676e85e4f27f9a788308c8fe06d03b7b51
Samuel Heinemann <samuelehp04@gmail.com>
2025-01-23 17:39:39 -0300

    feat(custom-errors): adiciona os erros customizados Service e MethodNotAllowed

--- infra/erros.js
+++ infra/erros.js
@@ -1,9 +1,45 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Erro interno não esperado", { cause });
     this.name = "InternalServerErros";
     this.action = "Entre em contato com o suporte";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Serviço indisponível no momento", { cause });
+    this.name = "ServiceError";
+    this.action = "Verifique o serviço e tente novamente";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para este endpoint");
+    this.name = "MethodNotAllowedError";
+    this.action = "Verifique o método HTTP enviado";
+    this.statusCode = 405;
   }
 
   toJSON() {
